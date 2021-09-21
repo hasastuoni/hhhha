a0d7f55a612f425a644f1257c7559058f531802e
Gediminas Dausynas <gediminas.dausynas@gmail.com>
2021-09-21 21:35:24 +0300

    New commit

--- Program.cs
+++ Program.cs
@@ -18,10 +18,43 @@ namespace LI1
             Console.WriteLine();
             int count = TaskUtils.Counter(allBook);
             Console.WriteLine("Senesnes knygos negu 2 metai:{0}", count);
+
+            // Print all authors.
             List<string> Authors = TaskUtils.FindAuthors(allBook);
-            InOutUtils.PrintAuthor(Authors);
+            //InOutUtils.PrintAuthor(Authors);
+
+            // Dictionary <string (author), int (repeated count)
+            Dictionary<string, int> AuthorCount = new Dictionary<string, int>();
+
+            for(int i = 0; i < Authors.Count; i++)
+            {
+                if(!AuthorCount.ContainsKey(Authors[i]))
+                {
+                    AuthorCount.Add(Authors[i], 1);
+                } else
+                {
+                    AuthorCount[Authors[i]] += 1;
+                }
+            }
+
+            int max = 0;
+            string authorMax = "";
+
+            foreach (KeyValuePair<string, int> kvp in AuthorCount)
+            {
+                if (Int32.Parse(kvp.Value.ToString()) > max)
+                {
+                    max = kvp.Value;
+                    authorMax = kvp.Key;
+                }
+            }
+
+            Console.WriteLine("Top Author: {0}", authorMax);
+
             Console.WriteLine();
- 
+
+            
+            while(true) { }
         }
     }
 }
